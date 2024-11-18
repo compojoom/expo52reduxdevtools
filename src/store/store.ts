bd658b02c7a697d1a509069219e45f86486f862b
Daniel Dimitrov <daniel@compojoom.com>
2024-11-18 17:45:04 +0100

    initial commit

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+import devToolsEnhancer from "redux-devtools-expo-dev-plugin";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from './counterSlice'
+
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+    },
+    devTools: false,
+    enhancers: (getDefaultEnhancers) =>
+        getDefaultEnhancers().concat(devToolsEnhancer()),
+});
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
